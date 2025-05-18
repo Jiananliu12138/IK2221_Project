e3c8b1aaee21de33542c9b663dc75b163954abc7
ik2221vt25g07 <ik2221vt25g07@nslrack27.nslab>
2025-05-18 23:55:27 +0200

    finish task1

--- frontend/task1_implemention.py
+++ frontend/task1_implemention.py
@@ -3,7 +3,7 @@ import pandas as pd
 import chat_session
 import matplotlib.pyplot as plt
 from transformers import AutoTokenizer, AutoModel
-from tools import read_chunks, generate_requests
+from tools import read_chunks, generate_requests, generate_requests_shuffle
 
 MODEL_NAME = "Qwen/Qwen2.5-1.5B-Instruct"
 IP1 = "192.168.2.27"
@@ -36,6 +36,8 @@ questions = [
     "What is the Abstract of this document?",
     "What is the Introduction of this document?",
     "What is the Conclusion of this document?",
+    "What is the Background of this document?",
+    "What is the KV Cache Manager?",
 ]
 
 selected_keys = ["vllm_short", "vllm_medium", "vllm"]
@@ -76,14 +78,14 @@ for repeat in range(2):
         print("="*40)
 
     df = pd.DataFrame(results)
-    df.to_csv(f"task1_1_{repeat} results.csv", index=False)
+    df.to_csv(f"task1_result/task1_1_{repeat} results.csv", index=False)
 
     df_short = df[df["context_id"] == "vllm_short"]
     df_medium = df[df["context_id"] == "vllm_medium"]
     df_long = df[df["context_id"] == "vllm"]
     grouped_short = df_short.groupby("context_id")[["latency", "total_length"]].mean().reset_index()
-    grouped_medium = df_short.groupby("context_id")[["latency", "total_length"]].mean().reset_index()
-    grouped_long = df_short.groupby("context_id")[["latency", "total_length"]].mean().reset_index()
+    grouped_medium = df_medium.groupby("context_id")[["latency", "total_length"]].mean().reset_index()
+    grouped_long = df_long.groupby("context_id")[["latency", "total_length"]].mean().reset_index()
 
     plt.figure(figsize=(8, 5))
     plt.plot(grouped_short["total_length"], grouped_short["latency"], marker='o', label="vllm_short")
@@ -96,5 +98,63 @@ for repeat in range(2):
     plt.grid(True)
     plt.tight_layout()
     plt.show()
-    plt.savefig(f"task1_1_{repeat}_plot.png")
+    plt.savefig(f"task1_result/task1_1_{repeat}_plot.png")
+
+#shuffle不能一块跑，token好像累计算的会过大
+# for repeat in range(2,4):
+#     results = []
+#     for req in generate_requests_shuffle(task1_chunk, questions):
+#         session.set_context([system_prompt, req["context"]])
+#         start = time.perf_counter()
+#         try:
+#             response = "".join([x for x in session.chat(req["question"])])
+#         except Exception as e:
+#             print(f"Error: {e}")
+#             continue
+#         end = time.perf_counter()
+#         latency = end - start
+#         result = {
+#             "context_id": req["context_id"],
+#             "question": req["question"],
+#             "response": response,
+#             "latency": latency,
+#             "context_length": len(req["context"]),
+#             "question_length": len(req["question"]),
+#             "total_length": len(req["context"]) + len(req["question"])
+#         }
+#         results.append(result)
+#         print("="*40)
+#         print(f"Repeat: {repeat}")
+#         print(f"Context ID: {result['context_id']}")
+#         print(f"Question: {result['question']}")
+#         print(f"Response: {result['response'][:20]}...")
+#         print(f"Latency: {result['latency']:.3f} s")
+#         print(f"Context Length: {result['context_length']}")
+#         print(f"Question Length: {result['question_length']}")
+#         print(f"Total Length: {result['total_length']}")
+#         print(f"Throughput: {1/result['latency']:.2f} req/s")
+#         print("="*40)
+
+#     df = pd.DataFrame(results)
+#     df.to_csv(f"task1_result/task1_1_{repeat} results.csv", index=False)
+
+#     df_short = df[df["context_id"] == "vllm_short"]
+#     df_medium = df[df["context_id"] == "vllm_medium"]
+#     df_long = df[df["context_id"] == "vllm"]
+#     grouped_short = df_short.groupby("context_id")[["latency", "total_length"]].mean().reset_index()
+#     grouped_medium = df_medium.groupby("context_id")[["latency", "total_length"]].mean().reset_index()
+#     grouped_long = df_long.groupby("context_id")[["latency", "total_length"]].mean().reset_index()
+
+#     plt.figure(figsize=(8, 5))
+#     plt.plot(grouped_short["total_length"], grouped_short["latency"], marker='o', label="vllm_short")
+#     plt.plot(grouped_medium["total_length"], grouped_medium["latency"], marker='o', label="vllm_medium")
+#     plt.plot(grouped_long["total_length"], grouped_long["latency"], marker='o', label="vllm_long")
+#     plt.xlabel("Total Length")
+#     plt.ylabel("Average Latency (s)")
+#     plt.title("Latency vs Total Length for Different Contexts Lengths")
+#     plt.legend()
+#     plt.grid(True)
+#     plt.tight_layout()
+#     plt.show()
+#     plt.savefig(f"task1_result/task1_1_{repeat}_plot.png")
 
